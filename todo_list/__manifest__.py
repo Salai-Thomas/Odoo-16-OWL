34edaab59196a61042a2609f646d120b6f16907f
SalaiThomas <salaithomas582@gmail.com>
2024-11-05 01:14:25 +0630

    init todo_list

---
+++ todo_list/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'OWL To Do List',
+    'version' : '1.0',
+    'summary': 'OWL To Do List',
+    'sequence': -1,
+    'description': """OWL To Do List""",
+    'category': 'OWL',
+    'depends' : ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/todo_list.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+}
